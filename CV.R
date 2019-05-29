0e1d147b36376de088b14167f04516f99e1766bd
ChenDouDouDou <48283563+ChenDouDouDou@users.noreply.github.com>
2019-05-29 13:18:08 +0800

    Update CV.R

--- CV.R
+++ CV.R
@@ -5,25 +5,18 @@ library('randomForest')
 library('stringr')
 rm(list=ls())
 
-f<-read.csv(file='SkempiM_1752.txt',header = TRUE, sep = '\t')
+f<-read.csv(file='SkempiM.txt',header = TRUE, sep = '\t')
 # rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutated.Chain,f$Mutation,f$label_dataset,sep = '_')  # SkempiS
 rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutation,f$label_dataset,sep = '_')  # SkempiM
 
-# label <- 'DDGexp~dE_vdw_wt+dE_vdw_mut+dG_solv_mut+dG_solv_wt+dPro_mut+dPro_wt+SA_com_wt+SA_part_wt+CS+ddG_fold'         # model features of single mutation
-label <- 'DDGexp~dE_vdw_wt+dE_vdw_mut+dG_solv_mut+dG_solv_wt+ddG_fold+CS+ddE_elec_.site.site.+ddrSA'                      # model features of multiple mutation
+# label <- 'DDGexp~ddE_vdw+ddG_solv+ddG_fold+SA_com_wt+SA_part_wt+CS+N_cont_wt'  # model features of single mutation
+label <- 'DDGexp~ddE_vdw+ddG_solv+ddG_fold+SA_com_wt+SA_part_wt+CS+E_cont_wt'  # model features of multiple mutation
 
 #### Leave-One-Complex-Out #####
 pdblist <- unique(f$PDB.id)
 for (pdb in pdblist) {
   train.f <- subset(f,f$PDB.id!=pdb)
   test.f  <- subset(f,f$PDB.id==pdb)
-  ########################## Linear regression ##
-  train.f.fit <- lm(as.formula(label),data=train.f)
-  pred.test.f.L <- predict(train.f.fit,test.f)
-  test.f$pred.test.f.L <- pred.test.f.L
-  filetitle.L <- paste('CrossValidation/MLR_LOO_',pdb,'.txt',sep = '')
-  write.table(pred.test.f.L,filetitle.L,quote = FALSE,row.names = TRUE,
-              col.names = FALSE,sep = '\t')
   ########################## Random Forest ##
   set.seed(100)
   train.f.rf <- randomForest(as.formula(label), data = train.f)
@@ -33,7 +26,6 @@ for (pdb in pdblist) {
   write.table(pred.test.f.R,filetitle.R,quote = FALSE,row.names = TRUE,
               col.names = FALSE,sep = '\t')
 }
-
 
 #### Leave-One-Type-Out ####
 f$label_traintest <- rownames(f)
@@ -50,12 +42,6 @@ for (pdb in pdblist){
     }
   }
   train.f <- f[!f$label_traintest %in% test.f$label_traintest,]
-  ########################## Linear regression ##
-  train.f.lr <- lm(as.formula(label),data=train.f)
-  pred.test.f.L <- predict(train.f.lr,data_pdb)
-  filetitle.L <- paste('CrossValidation/MLR_LBSO_',pdb,'.txt',sep = '')
-  write.table(pred.test.f.L,filetitle.L,quote = FALSE,row.names = TRUE,
-              col.names = FALSE,sep = '\t')
   ########################## Random Forest ##
   set.seed(100)
   train.f.rf <- randomForest(as.formula(label), data = train.f)

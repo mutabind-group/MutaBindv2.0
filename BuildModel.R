7ae8a8a5517be033caff79e48bef747261bfeb73
ChenDouDouDou <48283563+ChenDouDouDou@users.noreply.github.com>
2019-05-30 09:39:40 +0800

    Update BuildModel.R

--- BuildModel.R
+++ BuildModel.R
@@ -4,8 +4,8 @@ library(randomForest)
 setwd("C:/Users/ChenDouDou/Downloads/MutaBindv2.0-master/")     # can change
 
 f <- read.table(file="SkempiS.txt", header = TRUE, sep = '\t')
-rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutated.Chain,f$Mutation,f$label_dataset,sep = '_')                # for single mutation
-# rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutation,f$label_dataset,sep = '_')                              # for multiple mutation
+rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutated.Chain,f$Mutation.s._cleaned,f$label_dataset,sep = '_')                # for single mutation
+# rownames(f) <- paste(f$PDB.id,f$Partner1,f$Partner2,f$Mutation.s._cleaned,f$label_dataset,sep = '_')                              # for multiple mutation
 c(dim(f)[1],length(unique(f$PDB.id)))
 
 label <- 'DDGexp~ddE_vdw+ddG_solv+ddG_fold+SA_com_wt+SA_part_wt+CS+N_cont_wt'  # model features of single mutation
